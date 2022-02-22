fcfdaacb1cfd9954a538b3f627f562502837fad4
olle-dahlberg-nti-johanneberg <95086872+olle-dahlberg-nti-johanneberg@users.noreply.github.com>
2022-02-22 21:59:12 +0100

    Gjort om hela article 2 till en snyggare design som visar en produkt i taget.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 
 const header = document.getElementById("header");
 const img = document.getElementById('img');
-const burgerbutton = document.querySelector('#hamburgermenu')
+const burgerbutton = document.querySelector('#svg-bag')
 const nav = document.querySelector('.pop-down-menu')
 const btt = document.querySelector('.backtotop')
+
+const c1 = document.querySelector('#card1')
+const c2 = document.querySelector('#card2')
+const c3 = document.querySelector('#card3')
+
+
+
+const airpodsmax = document.querySelector('#airpodsmax')
+const airpods = document.querySelector('#airpods')
+
 var scrolled2 = 0
     var lastScrollTop = 0;
 
@@ -19,7 +29,7 @@ window.addEventListener('scroll', () => {
     nav.classList.remove('nav-active')
     burgerbutton.classList.remove('toggle')
 
-    if (scrolled >= ((scrollable * 40) / (620 - 100))) {
+    if (scrolled >= ((scrollable * 40) / (820 - 100))) {
         header.classList.add('scrolled');
         img.style.filter = `blur(${scrolled / 100}px)`
     } 
@@ -32,7 +42,32 @@ window.addEventListener('scroll', () => {
     }
 
 
-    if (scrolled >= ((scrollable * 420) / (620 - 100))) {
+    // airpodsmax in
+    if  (scrolled >= ((scrollable * 140) / (820 - 100)) && scrolled <= ((scrollable * 350) / (820 - 100))) {
+        airpodsmax.classList.add('visible')
+        c1.classList.add('visible')
+    }
+    else {
+        c1.classList.remove('visible')
+        airpodsmax.classList.remove('visible')
+    }
+
+
+    // airpods in, svart bakgrund
+    if  (scrolled >= ((scrollable * 340) / (820 - 100)) && scrolled <= ((scrollable * 550) / (820 - 100))) {
+        airpods.classList.add('visible')
+        c2.classList.add('visible')
+      
+    }
+    else {
+        c2.classList.remove('visible')
+        airpods.classList.remove('visible')
+      
+    }
+
+
+    // scroll up button in 
+    if (scrolled >= ((scrollable * 720) / (820 - 100))) {
         btt.classList.add('show');
     } 
     else {
